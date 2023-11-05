528b63790cf5b1e64818047d7342d325acf3a348
Jacob Rast <142756798+Rastifer@users.noreply.github.com>
2023-11-04 20:30:25 -0500

    Update script.js
    updated the script

--- script/script.js
+++ script/script.js
@@ -1,3 +1,60 @@
+$(function () {
+    let animal = [
+        ["dog", ["Labrador Retriever", "German Shepherd", "Dobermann", "English Bulldog"]],
+        ["bird", ["Bald Eagle", "Falcon", "Hawk", "Mockingbird"]],
+        ["bear", ["Grizzly Bear", "Polar Bear", "Black Bear", "Panda"]],
+        ["cat", ["Lion", "Tiger", "Cheetah", "Siamese"]],
+        ["insect", ["Dragonfly", "Dung Beetle", "Hornet", "Butterfly"]]
+    ];
+
+
+
+$("#animalType").on("change", function (e) {
+    //enables the animal dropdown
+    $("#animal").prop("disabled", false);
+
+    let inputVal = this.value;
+
+    console.log(inputVal);
+
+    //loop though array of animals
+    $.each(animal, function (key, value) {
+      //match animal to user selected
+      if (inputVal === value[0]) {
+        console.log(
+          "value[0]:" + value[0] + ", key:" + key + "value: " + value
+        );
+        $.each(value, function (nestKey, nestValue) {
+          // console.log(nestKey);
+
+          switch (nestKey) {
+            case 0:
+              $("label[for=animal]").text(nestValue);
+              $("#animal").empty();
+              $("#animal").append(
+                $("<option>").text(`select a ${nestValue} `)
+              );
+              break;
+            case 1:
+              $.each(nestValue, function (nameKey, nameValue) {
+                console.log(nameKey, nameValue);
+
+                $("#animal").append(
+                  $("<option>").val(nameValue).text(nameValue)
+                );
+              });
+              break;
+          }
+        });
+      }
+    });
+  });
+
+
+
+
+
+
 var checkboxes = document.querySelectorAll('input[type="checkbox"]');
     var maxAllowed = 3;
 
@@ -9,3 +66,4 @@ var checkboxes = document.querySelectorAll('input[type="checkbox"]');
             }
         });
     }
+});
